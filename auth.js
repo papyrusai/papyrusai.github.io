fb62b4920c08ea0e0728dad655c114b058edcdea
ini <papyrusai.info@gmail.com>
2025-01-24 01:41:25 +0100

    callback route update 2

--- auth.js
+++ auth.js
@@ -52,7 +52,7 @@ async function findUserByGoogleId(googleId) {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://https://papyrusai-github-io.onrender.com/auth/google/callback'
+  callbackURL: 'https://papyrusai-github-io.onrender.com/auth/google/callback'
 },
 async (token, tokenSecret, profile, done) => {
   try {
